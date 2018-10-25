f044b4370ca0bf779b95169add9e9ab538c1c332
amazoradi <amazoradi@gmail.com>
2018-10-25 16:07:13 -0500

    file tree updated, event listener on submit button added and working

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -1,67 +1,57 @@
-// food api search options
+// variables
+let itineraryDiv = document.querySelector(".itinerary")
 
-  let BBQ = {
-    name: "BBQ",
-    id: 193
-  }
-  let Burger = {
-    name: "Burger",
-      id: 168
-  }
-  let Cajun = {
-    name: "Cajun",
-      id: 491
-  }
-  let Chinese = {
-    name: "Chinese",
-    id: 25
-  }
-  let Ethiopian = {
-    name: "Ethiopian",
-      id: 149
-  }
-  let Indian = {
-    name: "Indian",
-      id: 148
-  }
-  let Italian = {
-    name: "Italian",
-    id: 55
-  }
-  let Mexican = {
-    name: "Mexican",
-    id: 73
-  }
-  let Pizza = {
-    name: "Pizza",
-    id: 82
-  }
-  let Tapas = {
-    name: "Tapas",
-    id: 179
-  }
-
-console.log(Tapas.id)
 let foodSection = document.getElementById("foodOptions")
 let foodButton = document.getElementById("foodButton")
 
-foodButton.addEventListener("click", ()=> {
+
+// element creator
+function elementFactory(el, content, ...children) {
+  let element = document.createElement(el);
+  element.innerHTML = content || null
+  children.forEach(child => {
+    element.appendChild(child)
+  })
+  return element
+}
+
+// function to get the value ie the id of my food ethncity of the selection when the button is clicked
+foodButton.addEventListener("click", () => {
+  // targets the "value" in of my dropdown options
   let clickValue = foodSection.options[foodSection.selectedIndex].value
-  let searchTheFood = foodSearch(clickValue);
-  foodFetch(searchTheFood)
+  console.log(clickValue)
+  foodFetch(clickValue)
 })
 
-function foodFetch (food) {
-  if ()
- 
-}
-// let name = document.querySelector(".foodOptions").innerHTML
-// console.log(name)
 
 
-// fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&cuisines=${.id}`, {
-//   headers: {
-//     "Content-Type": "application/json",
-//     "user-key": "96eabc6c9e55bec13dea468806aa7e48"
-//   }
-// }))
+function foodFetch(clickValue) {
+    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&cuisines=${clickValue}`, {
+    headers: {
+      "Content-Type": "application/json",
+      "user-key": "96eabc6c9e55bec13dea468806aa7e48"
+    }
+  })
+    .then(food => food.json())
+    .then(foodData => {
+      foodData.restaurants.forEach((food) => {
+        let foodName = elementFactory("p", `${food.restaurant.name}`)
+        itineraryDiv.appendChild(foodName)
+      })
+    })
+  }
+
+
+
+
+
+
+// fetch("http://localhost:8088/restaurants")
+//     .then(food => food.json())
+//     .then(foodData => {
+//       foodData.forEach((food) => {
+//         // console.log(food.restaurant.name)
+//         let foodName = elementFactory("p", `${food.restaurant.name}, ${food.restaurant.location.address}. Rated ${food.restaurant.user_rating.aggregate_rating} out of 5`)
+//         itineraryDiv.appendChild(foodName)
+// })
+//     })
