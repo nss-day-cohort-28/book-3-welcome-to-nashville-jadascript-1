8027bf6cffbc03eed1bcdda36d2dc039878d2383
Alfonso Miranda <Gabmin21@hotmail.com>
2018-10-26 14:45:00 -0500

    get the fetch working on external api

--- src/scripts/meetUps/meetups.js
+++ src/scripts/meetUps/meetups.js
@@ -1,60 +1,49 @@
-// This function will populate the results page when the button is clicked on meetups
 
-function meetUpsResultsPage() {
 
-  fetch("http://localhost:3000/events")
-    .then((eventBriteData) => eventBriteData.json())
-    .then((eventsData) => {
-  
-      // -----------------------------------------------------------
-      let meetupslist = document.querySelector(".searchResultTable")
-  
-      for (let i = 0; i < eventsData.length; i++) {
-        let currentEvent = eventsData[i]
-        // let admissionFee = 'No'
-        // if (currentEvent.is_free) {
-        //   admissionFee = 'Yes'
-        // }
-        let admissionFee = currentEvent.is_free
-        if (currentEvent.venue.address.city === "Nashville" ){
-        // console.log(eventsData[i].venue.address.city)
-        if (currentEvent.is_free === true) {
-          let loopedData = currentEvent.venue.address.city
-          admissionFee = "Yes"
-  
-          console.log(admissionFee)
-  
-          // meetupslist.innerHTML += 
-          // `<h3>Event:</h3>
-          // <h2>${currentEvent.name.text}</h2>
-          // <h3>Starting Time:</h3>
-          // <p>${currentEvent.start.utc}</p>
-          // <h3>Ends at:</h3>
-          // <p>${currentEvent.end.utc}</p>
-          // <h3>Admission Fee:</h3>
-          // <p>$`
-        }
-        else if (currentEvent.is_free === false) {
-          admissionFee = "No"
-        }
-        meetupslist.innerHTML += 
-        `<h3>Event:</h3>
-        <h2>${currentEvent.name.text}</h2>
-        <h3>Starting Time:</h3>
-        <p>${currentEvent.start.utc}</p>
-        <h3>Ends at:</h3>
-        <p>${currentEvent.end.utc}</p>
-        <h3>Free Admission:</h3>
-        <p>${admissionFee}</p>`
+fetch(`https://www.eventbriteapi.com/v3/events/search/?location.longitude=-86.767960&sort_by=date&token=F4AWXXJ4FU34AD7CC42C&location.latitude=36.174465&page=2&expand=venue`)
+  .then((eventBriteData) => eventBriteData.json())
+  .then((eventsData) => {
+    let meetupslist = document.querySelector(".searchResultTable")
+
+    for (let i = 0; i < eventsData.events.length; i++) {
+      let currentEvent = eventsData.events[i]
+
+      // //         console.log(currentEvent)
+
+
+      let admissionFee = currentEvent.is_free
+      //       if (currentEvent.venue.address.city === "Nashville" ){
+
+
+      if (currentEvent.is_free === true) {
+        // //         let loopedData = currentEvent.venue.address.city
+        admissionFee = "Yes"
+
       }
+      else if (currentEvent.is_free === false) {
+        admissionFee = "No"
       }
-    })
-  }
+      meetupslist.innerHTML += `<h3>Event:</h3>
+              <h2>${currentEvent.name.text}</h2>
+              <h3>Starting Time:</h3>
+              <p>${currentEvent.start.utc}</p>
+              <h3>Ends at:</h3>
+              <p>${currentEvent.end.utc}</p>
+              <h3>Free Admission:</h3>
+              <p>${admissionFee}</p>`
+
+
+    }
+  })
+
   
-  meetUpsResultsPage()
+
+
+// }
+
+// meetUpsResultsPage()
   // ---------------------------------------------------------------
-  
+
   // step 2 
     // wrap this in function and have it be called by an event listener
     // make an event listener that does the above by a click 
-  
