1f58aada2949d4ffe93e987ac2c9a9d47caa50f6
justinavickers <justinav38@gmail.com>
2018-10-26 11:19:39 -0500

    fixed the external api, was missing results page in the for each

--- src/scripts/concertsJS/database.js
+++ src/scripts/concertsJS/database.js
@@ -4,52 +4,53 @@ let itineraryDiv = document.querySelector(".searchResultTable")
 let musicEvents = document.querySelector(".musicEvents")
 //where we perform the actual search
 //get input 
-    let venueSearchName = document.querySelector(".venueSearchName")  
-    
-    
-    //compare user input against venue name
-    //the event listener allows us to click on the button and perform a function
-    musicEvents.addEventListener("click", function(){
-      let userVenueInput = venueSearchName.value 
-     
-      fetch("https://api.songkick.com/api/3.0/metro_areas/11104/calendar.json?apikey=p8YGjn0x2SYsMtkJ&page=1&min_date=2018-10-29&max_date=2018-10-29")     
-      //gets data
-      .then(resultsPage => resultsPage.json())      //transforms to json
-      .then(resultsPage =>{     //
-        document.querySelector(".searchResultTable").innerHTML = null;   //this sets the search reults back to nothing
-        return resultsPage}) //returns empty box
-      .then(resultsData => {                      //parses json
-        
-        
+let venueSearchName = document.querySelector(".venueSearchName")
+
+
+//compare user input against venue name
+//the event listener allows us to click on the button and perform a function
+musicEvents.addEventListener("click", function () {
+  let userVenueInput = venueSearchName.value
+
+  fetch("https://api.songkick.com/api/3.0/metro_areas/11104/calendar.json?apikey=p8YGjn0x2SYsMtkJ&page=1&min_date=2018-10-29&max_date=2018-10-29")
+    //gets data
+    .then(resultsPage => resultsPage.json())      //transforms to json
+    .then(resultsPage => {     //
+      document.querySelector(".searchResultTable").innerHTML = null;   //this sets the search reults back to nothing
+      return resultsPage
+    }) //returns empty box
+    .then(resultsData => {                      //parses json
+
+
       console.log(resultsData)
-      
-      resultsData.results.event.forEach((result) => {
+
+      resultsData.resultsPage.results.event.forEach((result) => {
         let venueName = elementFactory("p", result.venue.displayName)
-        console.log("venueName",venueName)
-        let artistName = elementFactory("p",result.performance[0].displayName)
+        console.log("venueName", venueName)
+        let artistName = elementFactory("p", result.performance[0].displayName)
         console.log(artistName)
         let eventTime = elementFactory("p", result.start.time)
-        if(userVenueInput === result.venue.displayName) {
+        if (userVenueInput === result.venue.displayName) {
           itineraryDiv.appendChild(venueName)
           itineraryDiv.appendChild(artistName)
           itineraryDiv.appendChild(eventTime)
 
         }
-        
-  
+
+
       })
-      
+
     })
-  })
-  
-
-  
-
-  
+})
 
 
 
 
-  
+
+
+
+
+
+
 
 
