8501a4380f1d9edbeefc5f77fef2b7e8ae9e1d95
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-10-25 22:51:22 -0500

    added custom fetch calls that fetch the data according to each search parameter in the parks dropdown

--- src/scripts/parksJS/database.js
+++ src/scripts/parksJS/database.js
@@ -31,14 +31,14 @@ let resultsDiv = document.querySelector(".searchResultTable")
 // Fetch functions that call our DOM function that creates the HTML element
 
 // Insert park name, address, and year established into the itinerary Div
-fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT")
-  .then(parks => parks.json())
-  .then(parksData => {
-    parksData.forEach((park) => {
-      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
-      itineraryDiv.appendChild(parkName)
-    })
-  })
+// fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT")
+//   .then(parks => parks.json())
+//   .then(parksData => {
+//     parksData.forEach((park) => {
+//       let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+//       itineraryDiv.appendChild(parkName)
+//     })
+//   })
 
   //Insert park name, address, and year established into the Results Div
   // fetch("http://localhost:8088/parks")
--- src/scripts/parksJS/main.js
+++ src/scripts/parksJS/main.js
@@ -1,2 +1,81 @@
-let playground = "playground=Yes"
+let playgrounds = "playground=Yes"
+
+fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&playground=Yes")
+  .then(parks => parks.json())
+  .then(parksData => {
+    parksData.forEach((park) => {
+      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+      resultsDiv.appendChild(parkName)
+    })
+  })
+
+let picnicShelters = "picnic_shelters=Yes"
+
+fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&picnic_shelters=Yes")
+  .then(parks => parks.json())
+  .then(parksData => {
+    parksData.forEach((park) => {
+      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+      resultsDiv.appendChild(parkName)
+    })
+  })
+
+let walkJogPaths = "walk_jog_paths=Yes"
+
+fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&walk_jog_paths=Yes")
+  .then(parks => parks.json())
+  .then(parksData => {
+    parksData.forEach((park) => {
+      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+      resultsDiv.appendChild(parkName)
+    })
+  })
+
+let hikingTrails = "hiking_trails=Yes"
+
+fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&hiking_trails=Yes")
+  .then(parks => parks.json())
+  .then(parksData => {
+    parksData.forEach((park) => {
+      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+      resultsDiv.appendChild(parkName)
+    })
+  })
+
+let dogPark = "dog_park=Yes"
+
+fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&dog_park=Yes")
+  .then(parks => parks.json())
+  .then(parksData => {
+    parksData.forEach((park) => {
+      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+      resultsDiv.appendChild(parkName)
+    })
+  })
+
+  let baseballFields = "baseball_fields=Yes"
+
+  fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&baseball_fields=Yes")
+  .then(parks => parks.json())
+  .then(parksData => {
+    parksData.forEach((park) => {
+      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+      resultsDiv.appendChild(parkName)
+    })
+  })
+
+  let basketballCourts = "basketball_courts=Yes"
+
+  fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&basketball_courts=Yes")
+  .then(parks => parks.json())
+  .then(parksData => {
+    parksData.forEach((park) => {
+      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+      resultsDiv.appendChild(parkName)
+    })
+  })
+
+  
+
+
 
