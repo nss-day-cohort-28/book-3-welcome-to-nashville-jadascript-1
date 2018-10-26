79a10f66884c328acc9a003325e43a85a4676aea
Alfonso Miranda <Gabmin21@hotmail.com>
2018-10-26 16:10:23 -0500

    add event listener to event.js on meetups folder, button now works, only for octoer and november, will work on december tonight.

--- src/scripts/meetUps/event.js
+++ src/scripts/meetUps/event.js
@@ -1,1 +1,13 @@
 // event listeners
+let meetUpsButton = document.querySelector(".meetUpsButton")
+
+meetUpsButton.addEventListener("click", () => {
+  // targets the value in my dropdown options
+  let formDateValue = document.querySelector("#meetUpDates").value
+  let splitDate = formDateValue.split(",")
+  console.log(splitDate)
+    getDate(splitDate)
+    
+  })
+
+  
