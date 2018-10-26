75f04dc88e996b7b07e578d7c281beca245891f7
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-10-25 23:56:43 -0500

    began work on my event listeners for the park search button. Refactoring is needed for it to work, but it's getting close

--- src/scripts/parksJS/event.js
+++ src/scripts/parksJS/event.js
@@ -1,1 +1,24 @@
-// event listeners
+// function to get the value of the search parameter of my park feature when the search button is clicked
+parkButton.addEventListener("click", () => {
+// targets the value in my dropdown options
+  let clickValue = parkSection.options[parkSection.selectedIndex].value
+  console.log(clickValue)
+  parkFetch(clickValue)
+})
+
+function parkFetch(clickValue) {
+  fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&${clickValue}`, {
+  headers: {
+    "Content-Type": "application/json",
+    "user-key": "vx0odMXRoly9l5yCnqWKCJykT"
+  }
+})
+  .then(parks => parks.json())
+  .then(parksData => {
+    parksData.forEach((park) => {
+      let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+      itineraryDiv.appendChild(parkName)
+    })
+  })
+
+}
