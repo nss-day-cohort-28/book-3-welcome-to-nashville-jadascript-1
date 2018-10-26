fb5d2b40c19d20408a9ddaa707a77f2cfb751fac
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-10-26 11:32:30 -0500

    made some progress on save button, still needs refactoring

--- src/scripts/parksJS/database.js
+++ src/scripts/parksJS/database.js
@@ -1,24 +1,4 @@
-// Generic fetch function just for an example
 
-//  fetch("http://localhost:8088/food")
-//   .then(foods => foods.json())
-//   .then(parsedFoods => {
-//     parsedFoods.forEach((food) => {
-//       localFood.push(food)
-//       fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
-//         .then(response => response.json())
-//         .then(productInfo => {
-//           // calling our element creting function with our data that has been extracted from the local and external API
-//           let title = elementFactory("h2", food.name)
-//           let subtitle = elementFactory("h3", food.ethnicity)
-//           let ingredients = elementFactory("p", productInfo.product.ingredients_text)
-//           let nutrients = elementFactory("p", `Calories per serving: ${productInfo.product.nutriments.energy_serving}. Fat per serving ${productInfo.product.nutriments.fat_serving}. Sugar per serving: ${productInfo.product.nutriments.sugars_serving}. `)
-//           let foodCard = elementFactory("div", null, title, subtitle,ingredients, nutrients)
-//           fragment.appendChild(foodCard)
-//           foodlist.appendChild(fragment)
-//         })
-//     })
-//   });
 
 // Target Elements
 
--- src/scripts/parksJS/event.js
+++ src/scripts/parksJS/event.js
@@ -1,10 +1,24 @@
 // function to get the value of the search parameter of my park feature when the search button is clicked
-parkButton.addEventListener("click", () => {
-// targets the value in my dropdown options
-  let clickValue = parkSection.options[parkSection.selectedIndex].value
-  console.log(clickValue)
-  parkFetch(clickValue)
-})
+
+// I don't even need this now because I'm using an "onchange" feature in my index dropdown
+
+// parkButton.addEventListener("click", () => {
+// // targets the value in my dropdown options
+//   let clickValue = parkSection.options[parkSection.selectedIndex].value
+//   console.log(clickValue)
+//   parkFetch(clickValue)
+// })
+
+function elementFactory(el, content, ...children) {
+  let element = document.createElement(el);
+  element.innerHTML = content || null
+  children.forEach(child => {
+    element.appendChild(child)
+  })
+  return element
+}
+
+
 
 function parkFetch(clickValue) {
  return fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&${clickValue}`)
@@ -14,7 +28,20 @@ function parkFetch(clickValue) {
     return park})
   .then(parksData => parksData.forEach((park) => {
     console.log(park)
-    let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+    let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established} <button onclick="parkFetchItin(event.target.value)">Save</button>`)
     resultsDiv.appendChild(parkName)
   }))
 }
+
+function parkFetchItin(clickValue) {
+  return fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=vx0odMXRoly9l5yCnqWKCJykT&${clickValue}`)
+   .then(park => park.json())
+   .then(park =>{
+     document.querySelector(".searchResultTable").innerHTML = null;
+     return park})
+   .then(parksData => parksData.forEach((park) => {
+     console.log(park)
+     let parkName = elementFactory("p", `${park.park_name}- Address: ${park.mapped_location_address}. Year Established: ${park.year_established}`)
+     itineraryDiv.appendChild(parkName)
+   }))
+ }
