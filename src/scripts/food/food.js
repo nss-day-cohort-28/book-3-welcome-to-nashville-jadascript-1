e99a523c511867e92aadbb17178f735e1662e2c1
amazoradi <amazoradi@gmail.com>
2018-10-27 17:18:52 -0500

    cleaned up code, got rid of console logs

--- src/scripts/food/food.js
+++ src/scripts/food/food.js
@@ -1,21 +1,15 @@
-
-
 let foodSection = document.getElementById("foodOptions")
 let foodButton = document.getElementById("foodButton")
-
 
 
 // function to get the value ie the id of my food ethncity of the selection when the button is clicked
 foodButton.addEventListener("click", () => {
   // targets the "value" in of my dropdown options
   let clickValue = foodSection.options[foodSection.selectedIndex].value
-  // console.log(clickValue)
   foodFetch(clickValue)
 })
 
-
 // function to fetch the data from the external API based on what food ethnicity was chosen
-
 function foodFetch(clickValue) {
   fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&cuisines=${clickValue}&count=6`, {
     headers: {
@@ -32,12 +26,9 @@ function foodFetch(clickValue) {
     // creates p, buttons and divs for all results and can give unique ids to our fetched data
     .then(foodData => {
       foodData.restaurants.forEach((food) => {
-        
         let foodName = elementFactory("p", `${food.restaurant.name}, ${food.restaurant.location.address}. Rating ${food.restaurant.user_rating.aggregate_rating} out of 5.`, null, `food-para-${food.restaurant.id}` )
         let saveButton = elementFactory("button", "Save", "saveButton", `food-button-${food.restaurant.id}`)
-        console.log(food.restaurant.id)
         let searchResultItem = elementFactory("div", null, null, `food-search-div-${food.restaurant.id}`, foodName, saveButton)
-        // itineraryDiv.appendChild(foodName)
         fragment.appendChild(searchResultItem)
         resultsDiv.appendChild(fragment)
       }) 
@@ -68,56 +59,6 @@ function addToItinerary(resultDiv) {
 }
 
 
-
-
-
-
-
-
-
-
-
-// // function that i hope will loop through all my save buttons
-// // function itineraryListeners() {
-// //   let buttonOfSave = document.querySelectorAll(".saveButton")
-// //   buttonOfSave.forEach((button) => {
-
-// //     // then add anclick event
-//     button.addEventListener("click", (selectedButton) => {
-//       // this console log isnt working
-//       console.log("clicked") 
-//       console.log(selectedButton.target.id.length)
-//       // the gets the unique part of the id from each button
-//       let foodId = selectedButton.target.id.substring(12, 19);
-//       // neither is this console log
-//       console.log(selectedButton.target)
-// // it then takes the unique id and puts it in the next function
-//       pullSelectedElement(foodId);
-//     });
-//   });
-// }
-
-
-// // this function takes the id from the last function
-// function pullSelectedElement(ID) {
-//   // and finds the paragraph with the same unqiue id
-//   let selectedFood = document.querySelector(`#food-para-${ID}`);
-//   // and puts it in the next function
-//   addToItinerary(selectedFood);
-// };
-
-// // this function should take in our selected Paragraph and appended it to a div with the class foodItinerary in our interary div
-// function addToItinerary(resultDiv) {
-//   let foodItineraryItem = document.querySelector(".foodItinerary");
-//     foodItineraryItem.appendChild(resultDiv);
-//     return foodItineraryItem
-// }
-// itineraryListeners()
-
-
-
-
-
-
-
-
+function patchToObject () {
+  
+}
