c7481711b33e7298db1797d5e986628581c43400
amazoradi <amazoradi@gmail.com>
2018-10-29 11:05:56 -0500

    added style, delete commented out code

--- src/scripts/food/food.js
+++ src/scripts/food/food.js
@@ -26,46 +26,36 @@ function foodFetch(clickValue) {
     // creates p, buttons and divs for all results and can give unique ids to our fetched data
     .then(foodData => {
       foodData.restaurants.forEach((food) => {
-        let foodName = elementFactory("p", `${food.restaurant.name}, ${food.restaurant.location.address}. Rating ${food.restaurant.user_rating.aggregate_rating} out of 5.`, null, `food-para-${food.restaurant.id}` )
+        let foodName = elementFactory("p", `${food.restaurant.name}, ${food.restaurant.location.address}. Rating ${food.restaurant.user_rating.aggregate_rating} out of 5.`, null, `food-para-${food.restaurant.id}`)
         let saveButton = elementFactory("button", "Save", "saveButton", `food-button-${food.restaurant.id}`)
         let searchResultItem = elementFactory("div", null, null, `food-search-div-${food.restaurant.id}`, foodName, saveButton)
         fragment.appendChild(searchResultItem)
         resultsDiv.appendChild(fragment)
-      }) 
-       eventlisten()
-    }) 
+      })
+      eventlisten()
+    })
 }
-// let itinObject
 
-// fetch("http://localhost:8088/itinerary")
-//   .then((data) => data.json())
-//   .then((itineraryDb) => {
-//     itinObject = itineraryDb
-//   })
-
-// let finalButton = document.querySelectorAll(".finalButton")
 // adds an click event on the save button and targets the info paragraph
-function eventlisten () {
+function eventlisten() {
   for (let i = 0; i < 6; i++) {
-  let buttonOfSave = document.querySelectorAll(".saveButton")
-  buttonOfSave[i].addEventListener("click", () => {
-  let selectedFood = buttonOfSave[i].previousSibling
-  addToItinerary(selectedFood)
-  buttonOfSave[i].style.visibility = "hidden";
-  })
-    // addToLocal(itinObject)
-}
+    let buttonOfSave = document.querySelectorAll(".saveButton")
+    buttonOfSave[i].addEventListener("click", () => {
+      let selectedFood = buttonOfSave[i].previousSibling
+      addToItinerary(selectedFood)
+      buttonOfSave[i].style.visibility = "hidden";
+    })
+  }
 }
 
 // this function should take in our selected Paragraph and appended it to a div with the class foodItinerary in our interary div
 function addToItinerary(resultDiv) {
   let foodItineraryItem = document.querySelector("#foodItinerary");
-  // foodItineraryItem.innerHTML = "<button class='finalButton'>Save</button>"
+  foodItineraryItem.innerHTML =""
   foodItineraryItem.appendChild(resultDiv);
-
 }
 
-
+// event listener to last save button to create the json object
 function finalItineraryListen() {
   let finalSaveButton = document.querySelector(".finalButton")
   finalSaveButton.addEventListener("click", () => {
@@ -81,33 +71,8 @@ function finalItineraryListen() {
 }
 finalItineraryListen()
 
-// addToLocal = (data) => {
-//   console.log(JSON.stringify(data));
-//   let addLocal = new XMLHttpRequest();
-//   addLocal.open("POST", "http://localhost:8088/itinerary", true);
-//   addLocal.setRequestHeader('Content-Type', 'application/json');
-//   addLocal.setRequestHeader('Access-Control-Allow-Origin', '*');
-//   addLocal.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   addLocal.send(JSON.stringify(data));
-// }
 
-
-
-// function addToLocal (data) {
-//   let finalButton =document.querySelectorAll(".finalButton")
-//   finalButton.addEventListener("clcik", () => {
-//   return fetch("http://localhost:8088/itinerary", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(data)
-//   })
-//     .then(contacts => contacts.json())
-//   })
-
-// }
-
+// adding our data to the .json object
 const saveToItin = (data) => {
   return fetch("http://localhost:8088/itinerary/1", {
     method: "PUT",
